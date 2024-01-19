2ebb63bf34d6a8fa9767eb0a02e7219502b627bc
mvalladares-2019607 <134962132+mvalladares-2019607@users.noreply.github.com>
2024-01-19 11:06:43 -0600

    Calculadora

---
+++ assets/logica.js
@@ -0,0 +1,22 @@
+let valorPantalla = '';
+function limpiarPantalla(){
+    valorPantalla ='';
+    actualizarPantalla();
+}
+function agregarCaracter(char){
+    valorPantalla += char;
+    actualizarPantalla();
+}
+function calcular(){
+    try{
+        valorPantalla = eval(valorPantalla).toString();
+        actualizarPantalla();
+    }catch(e){
+        valorPantalla = 'Error :c'
+        actualizarPantalla();
+    }
+}
+function actualizarPantalla(){
+    document.getElementById('pantalla').value = valorPantalla;
+
+}
